0ae2b512981d5e80373fb9022a862ab96bebc2a9
thenileshnishad <thenileshnishad@gmail.com>
2024-03-13 22:26:32 +0530

    feat: Implement basic routing with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./App.css"
-import RegisterPage from "./pages/User/Register/RegisterPage"
+import Register from "./pages/User/Register/RegisterPage"
 import Login from "./pages/User/Login/LoginPage"
 
 const App = () => {
   return (
     <>
-      <RegisterPage />
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
